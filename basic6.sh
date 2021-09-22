9e1797fa194088ca5676e72fe1f393a107e04f16
J03K3R <joe@joeszone.co.uk>
2021-09-22 14:45:46 +0100

    created basic6.sh

---
+++ basic6.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This is a script that will solve the password challenge of Hack This Site basic level 6.
+# The password is dynamically generated so please replace the value of MyString with the password.
+MyString='62cf4;j='
+i=0
+base=0
+echo "Converting to ascii value"
+while (( i++ < ${#MyString} ))
+do
+   char=$(expr substr "$MyString" $i 1)
+    for j in `printf "%d" \'$char` ; do
+    	j=$((j+base))
+    	printf \\$(printf '%03o' $j)
+    	base=$((base-1))
+    done;
+done;
